b2f9e7bf6029b81238bbc338b188d3fc9f02010e
AulakhAccounts <rk.aulakhacc96@gmail.com>
2026-06-06 11:27:43 +0530

    Add Render environment variables

--- accounting_website/settings.py
+++ accounting_website/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ixy92rjn=5lvy1iji7%eity=haym#zqzskeg&0s1zowyi%*x+8'
+SECRET_KEY = os.environ.get(
+'SECRET_KEY',
+'django-insecure-ixy92rjn=5lvy1iji7%eity=haym#zqzskeg&0s1zowyi%*x+8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
